d37bc98abd58a3abcc7e1bd01fe034e444f89a7d
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-12-03 18:49:03 +0700

    fix predict request trouble

--- app.py
+++ app.py
@@ -15,23 +15,15 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Load model
-model = load_model(os.getenv('MODEL_PATH', 'model/WaterEye-v0.h5'))
+model_path = os.getenv('MODEL_PATH', 'model/WaterEye-v0.h5')
+model = load_model(model_path)
 model.make_predict_function()
 
 # Define labels
-## v0 Warna
 dic = {0: 'black', 1: 'blue', 2: 'brown', 3: 'clean', 4: 'green', 5: 'red', 6: 'yellow'}
-## v1,v2 Warna + Kejernihan
-# dic = {
-#     0: 'black-concentrated', 1: 'blue-concentrated', 2: 'brown-concentrated',
-#     3: 'black-clear', 4: 'blue-clear', 5: 'brown-clear',
-#     6: 'green-clear', 7: 'red-clear', 8: 'transparent-clear',
-#     9: 'transparent-concentrated', 10: 'yellow-clear', 11: 'green-concentrated',
-#     12: 'not_water', 13: 'red-concentrated', 14: 'yellow-concentrated'
-# }
 
 def predict_label(img_bytes):
-    # Load image resize, and normalize
+    # Load image, resize, and normalize
     img = image.load_img(io.BytesIO(img_bytes), target_size=(224, 224))
     img = image.img_to_array(img) / 255.0
     img = img.reshape(1, 224, 224, 3)
@@ -48,16 +40,18 @@ def health():
 @app.route("/predict", methods=['POST'])
 def predict():
     try:
-        # Check for JSON input
+        img_bytes = None  # Initialize img_bytes to ensure it's always defined
+        # Check for JSON input (base64 encoded image)
         if request.is_json:
             data = request.get_json()
-            if 'image_data' in data:
-                image_data = data['image_data']
-                image_data = image_data.split(',')[1]
-                image_bytes = base64.b64decode(image_data)
+            if 'image' in data:
+                image_data = data['image']
+                image_data = image_data.split(',')[1]  # Strip off the base64 prefix
+                img_bytes = base64.b64decode(image_data)
             else:
                 return jsonify({"error": "No image data provided"}), 400
-        elif 'file' in request.files:  # Handle file uploads
+        # Check for file uploads
+        elif 'file' in request.files:
             img = request.files['file']
             if img and allowed_file(img.filename):
                 img_bytes = img.read()
@@ -65,6 +59,10 @@ def predict():
                 return jsonify({"error": "Invalid file format"}), 400
         else:
             return jsonify({"error": "No image provided"}), 400
+
+        # Ensure img_bytes is not None before calling the prediction
+        if img_bytes is None:
+            return jsonify({"error": "No valid image data provided"}), 400
 
         # Predict label
         color_water, accuracy = predict_label(img_bytes)
